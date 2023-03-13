b92afde18fb267e3f99e22a4caade4b823e767a1
thais viana <thaisviana081@outlook.com>
2023-03-13 16:14:23 -0300

    1 - Crie a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,9 +1,24 @@
+import requests
+import time
+
+
 # Requisito 1
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, headers={"user-agent": "Fake user-agent"})
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
+
+
 def scrape_updates(html_content):
     """Seu código deve vir aqui"""
 
